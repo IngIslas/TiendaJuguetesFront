4e02cd4f12e4ad240abd7a1c3a9ac0f10a77721f
Javier Cruz Islas <javier.cruz.islas97@gmail.com>
2022-11-18 20:09:28 -0600

    Feat: Vista de Catalogo de juguetes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,88 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <!-- <HelloWorld /> -->
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-btn color="green" @click="isActive = true">Agregar Nuevo</v-btn>
+      </v-col>
+    </v-row>
+    <br />
+    <v-row>
+      <v-data-table :headers="headers"> </v-data-table>
+    </v-row>
+    <v-dialog :value="isActive" width="500" persistent>
+      <v-card maxwidth="500" width="500">
+        <v-toolbar color="primary" dark> Agregar nuevo Juguete </v-toolbar>
+        <v-text>
+          <v-form>
+            <v-container fluid pa-10>
+              <v-row>
+                <v-col>
+                  <v-text-field label="Nombre"></v-text-field>
+                </v-col>
+                <v-col>
+                  <v-text-field label="Precio" type="number"></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-text-field label="Compañia"></v-text-field>
+              </v-row>
+              <v-row>
+                <v-text-field label="Edad Maxima" type="number"></v-text-field>
+              </v-row>
+              <v-row>
+                <v-textarea label="Descripcion"> </v-textarea>
+              </v-row>
+            </v-container>
+          </v-form>
+        </v-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green" @click="Guardar">Guardar</v-btn>
+          <v-btn color="error" @click="isActive = false">Cancelar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+
+  data: () => ({
+    isActive: true,
+    headers: [
+      {
+        value: "Id",
+        text: "Id",
+      },
+      {
+        value: "Nombre",
+        text: "Nombre",
+      },
+      {
+        value: "Edad",
+        text: "Edad",
+      },
+      {
+        value: "Precio",
+        text: "Precio",
+      },
+      {
+        value: "Compañia",
+        text: "Compañia",
+      },
+      {
+        value: "actions",
+        text: "",
+      },
+    ],
+  }),
+  methods: {
+    Guardar() {
+      this.isActive = false;
+    },
   },
 };
 </script>
