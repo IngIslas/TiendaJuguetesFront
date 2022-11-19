9d536622c25028792ab43deb992dbdf489261d01
Javier Cruz Islas <javier.cruz.islas97@gmail.com>
2022-11-19 13:24:18 -0600

    Feat: Agregar axios a VueApp

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import axiosCors from "vue-axios-cors";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false;
-
+Vue.use(VueAxios, axios);
+Vue.use(axiosCors);
 new Vue({
   router,
   vuetify,
